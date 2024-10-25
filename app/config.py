58c07d142608b9ab26657125acff8d729a3431f3
A1astorrr <meshcheryakov.v@list.ru>
2024-10-25 22:05:25 +0300

    creating database and config

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+from pydantic import SecretStr, Field
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int = Field(ge=1, le=65535)
+    DB_USER: str
+    DB_PASS: SecretStr
+    DB_NAME: str
+    
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS.get_secret_value()}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    class Config:
+        env_file = ".env"
+        
+settings = Settings()
